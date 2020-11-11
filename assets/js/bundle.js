3cffdeff71f2f241aa6c265ef65867bec0470abb
/ <31470255+Cracked679@users.noreply.github.com>
2020-11-11 22:38:26 +0530

    Initial primitive completion still need reformatting.

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -42,3 +42,20 @@ $(document).ready(function () {
       .prepend("0");
   });
 });
+
+// maps
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const location = { lat: 28.5928785, lng: 77.3030458 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: location,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+  });
+}
